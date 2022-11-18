e453210e806c6d6c689af0790ad7adddfce1c60a
Ronia Peterson <113647910+RealZetaRiemann@users.noreply.github.com>
2022-11-18 12:12:12 -0500

    rough oauth sign in

---
+++ oauth/app.py
@@ -0,0 +1,48 @@
+"""Flask app for weatherspoons.com"""
+from flask import Flask, render_template, url_for, redirect
+from authlib.integrations.flask_client import OAuth
+import secrets
+
+app = Flask(__name__)
+
+app.secret_key = secrets.token_bytes(32)
+
+oauth = OAuth(app)
+
+app.config['GITHUB_CLIENT_ID'] = "8427d9c0a9294daa443a"
+app.config['GITHUB_CLIENT_SECRET'] = "f582a5bde4e3eedcf2128d91ff1dfb70433da4be"
+
+github = oauth.register (
+  name = 'github',
+    client_id = app.config["GITHUB_CLIENT_ID"],
+    client_secret = app.config["GITHUB_CLIENT_SECRET"],
+    access_token_url = 'https://github.com/login/oauth/access_token',
+    access_token_params = None,
+    authorize_url = 'https://github.com/login/oauth/authorize',
+    authorize_params = None,
+    api_base_url = 'https://api.github.com/',
+    client_kwargs = {'scope': 'user:email'},
+)
+
+# Default route
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+# Github login route
+@app.route('/login/github')
+def github_login():
+    github = oauth.create_client('github')
+    redirect_uri = url_for('github_authorize', _external=True)
+    return github.authorize_redirect(redirect_uri)
+
+# Github authorize route
+@app.route('/login/github/authorize')
+def github_authorize():
+    github = oauth.create_client('github')
+    token = github.authorize_access_token()
+    resp = github.get('user').json()
+    print(f"\n{resp}\n")
+    return "You are successfully signed in using github"
+
+app.run(host='localhost', port=5000)
